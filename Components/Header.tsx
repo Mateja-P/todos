73152188b590be7938178db71e747a55205cc9e7
Mateja <mateja@alideda.rs>
2023-01-26 11:01:33 +0100

    todos

--- Components/Header.tsx
+++ Components/Header.tsx
@@ -3,7 +3,7 @@ const Header = () => {
     <div className='bg-primary p-2 pl-10 sm:px-2'>
       <div className='flex justify-between w-full items-center'>
         <div className='text-white font-bold'>Logo</div>
-        <div className='flex justify-end'>
+        <div className='inline'>
           <form>
             <input
               className='p-1.5 placeholder:text-xs outline-0 w-80 sm:w-1/2'
