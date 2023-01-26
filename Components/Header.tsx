96b853823cdaead19f647f230f059b3eac3ba2fb
Mateja <mateja@alideda.rs>
2023-01-26 11:05:34 +0100

    todos

--- Components/Header.tsx
+++ Components/Header.tsx
@@ -3,14 +3,10 @@ const Header = () => {
     <div className='bg-primary p-2 pl-10 sm:px-2'>
       <div className='flex justify-between w-full items-center'>
         <div className='text-white font-bold'>Logo</div>
-        <div className='inline'>
-          <form className='inline'>
-            <input
-              className='p-1.5 placeholder:text-xs outline-0 w-80 md:w-auto'
-              placeholder='Search Todos'
-            />
-          </form>
-        </div>
+        <input
+          className='p-1.5 placeholder:text-xs outline-0 w-80 md:w-1/2'
+          placeholder='Search Todos'
+        />
       </div>
     </div>
   );
